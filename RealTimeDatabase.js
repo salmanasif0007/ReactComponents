7d91a80aaaffcd60c7c0a612b6399eff96fb2f86
Salman Asif <47418635+salmanasif0007@users.noreply.github.com>
2020-08-20 17:34:57 +0600

    Create RealTimeDatabase.js

---
+++ RealTimeDatabase.js
@@ -0,0 +1,44 @@
+            @@@@@@@@@@         write          @@@@@@@@@@
+            
+            
+            
+            
+  const messageRef = firebase.database().ref('Message');
+  const message = {
+      data:this.props.product
+  }; 
+  
+  messageRef.push(message);
+  
+  
+  
+              @@@@@@@@@@         read          @@@@@@@@@@
+              
+              
+              
+              
+              
+    const [todoList, setTodoList] = useState();
+    useEffect(() => {
+    const todoRef = firebase.database().ref('Message');
+    todoRef.on('value', (snapshot) => {
+        const todos = snapshot.val();
+        const todoList = [];
+        for (let id in todos) {
+        todoList.push({ id, ...todos[id] });
+        }
+        setTodoList(todoList);
+        
+    });
+    }, []);
+    
+    return (
+        
+        <div>
+    <Container  id="box" fluid>
+    <ScrollableFeed>
+                
+            {todoList
+            ? todoList.map((todo) =>
+            <Row key={todo.id} style={{marginTop:"10px"}}>
+                    {todo.name.photoURL ?(
